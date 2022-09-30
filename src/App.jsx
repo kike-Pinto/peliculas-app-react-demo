67e5d554cd32522dbe7971a32606cc2c3773ae83
kike-pinto <cripintort@gmail.com>
2022-09-30 16:08:27 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export function App() {
     <Router>
       <header>
         <Link to='/'>
-          <h1 className={styles.title}>Movies</h1>
+          <h1 className={styles.title}>💜💜Movies💜💜</h1>
         </Link>
       </header>
       <main>
